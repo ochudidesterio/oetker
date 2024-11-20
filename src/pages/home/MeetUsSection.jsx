20ce02896802f6dae1eab3b11a5c642ee5604613
ochudidesterio <desochudi@gmail.com>
2024-11-20 20:26:12 +0300

    fix

--- src/pages/home/MeetUsSection.jsx
+++ src/pages/home/MeetUsSection.jsx
@@ -4,38 +4,22 @@ import { MdPlayCircle } from "react-icons/md";
 import { FiInstagram } from "react-icons/fi";
 import { DiOpera } from "react-icons/di";
 
-
-
 const MeetUsSection = () => {
   const [visibleCount, setVisibleCount] = useState(1); // Initially visible items
   const [itemsToLoad, setItemsToLoad] = useState(1); // Default items to load
 
-  // Adjust itemsToLoad and grid layout based on screen size
   useEffect(() => {
     const updateItemsToLoad = () => {
       const width = window.innerWidth;
       if (width >= 1024) {
         setItemsToLoad(4); // xl: 4 items
-        setVisibleCount(4);
       } else if (width >= 768) {
         setItemsToLoad(3); // large: 3 items
-        setVisibleCount(3);
       } else if (width >= 540) {
         setItemsToLoad(2); // medium: 2 items
-        setVisibleCount(2);
       } else {
         setItemsToLoad(1); // small: 1 item
-        setVisibleCount(1);
       }
-
-    //   if(scrolled ){
-    //     setItemsToLoad(itemsToLoad)
-    //     setVisibleCount(visibleCount)
-    //   }
-    //   if(!scrolled){
-    //     setItemsToLoad(itemsToLoad)
-    //     setVisibleCount(visibleCount)
-    //   }
     };
 
     updateItemsToLoad();
@@ -47,7 +31,7 @@ const MeetUsSection = () => {
   }, []);
 
   const loadMore = () => {
-    setVisibleCount((prev) => prev + itemsToLoad); // Load items dynamically based on screen size
+    setVisibleCount((prev) => Math.min(prev + itemsToLoad, paginationData.length));
   };
 
   return (
@@ -89,11 +73,10 @@ const HoverCard = ({ item }) => {
 
   return (
     <div
-      className="relative   transition duration-300 cursor-pointer overflow-hidden"
+      className="relative transition duration-300 cursor-pointer overflow-hidden"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      {/* Default content */}
       {!isHovered && (
         <div className="w-full">
           <img
@@ -104,28 +87,33 @@ const HoverCard = ({ item }) => {
           <div className="absolute inset-0 flex items-center justify-center">
             <MdPlayCircle size={50} className="text-white opacity-70" />
           </div>
-          <div className="absolute flex justify-end bottom-2 right-2 ">
+          <div className="absolute flex justify-end bottom-2 right-2">
             <FiInstagram size={30} className="text-white opacity-70" />
           </div>
-          
         </div>
       )}
-
-      {/* Hover content */}
       {isHovered && (
-        <div className="absolute inset-0 bg-white flex flex-col items-center pt-5 ">
-          <h3 className="text-lg font-cardo font-semibold text-accent">{item.name}</h3>
-          <p className="text-center text-md font-cardo mt-2">{item.details[0].description}</p>
+        <div className="absolute inset-0 bg-white flex flex-col items-center pt-5">
+          <h3 className="text-lg font-cardo font-semibold text-accent">
+            {item.name}
+          </h3>
+          <p className="text-center text-md font-cardo mt-2">
+            {item.details[0].description}
+          </p>
           <div className="absolute w-full bottom-0">
             <div className="w-full h-[0.5px] bg-accent"></div>
-            <div className="w-full flex justify-between items-center  py-1 pr-2 pl-3">
-                <div className="flex items-center ">
-                    <DiOpera size={20} className="text-accent" />
-                    <h2 className="text-accent uppercase text-sm font-cardo">ETKERCOLLECTION</h2>
-                </div>
-                <div>
-                    <h2 className="text-gray-300 uppercase text-sm font-cardo">{item.details[0].duration}</h2>
-                </div>
+            <div className="w-full flex justify-between items-center py-1 pr-2 pl-3">
+              <div className="flex items-center">
+                <DiOpera size={20} className="text-accent" />
+                <h2 className="text-accent uppercase text-sm font-cardo">
+                  ETKERCOLLECTION
+                </h2>
+              </div>
+              <div>
+                <h2 className="text-gray-300 uppercase text-sm font-cardo">
+                  {item.details[0].duration}
+                </h2>
+              </div>
             </div>
           </div>
         </div>
